bd2f6d90a2eb8e7393d102e9b59cbba46c119bb0
sabashukvaniii <sshukvanii@gmail.com>
2024-06-27 20:24:44 +0400

    Add files via upload

---
+++ level12/classwork.py
@@ -0,0 +1,9 @@
+age = int(input())
+if age <13:
+     print("you are a kid")
+     
+elif age > 13 and age < 20:
+     print("you are a teenager")
+
+elif age > 20:
+     print("you are a grown up")
