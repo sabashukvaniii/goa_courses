9531cda38d7a7e33de0c50bb2e61c84477cdfee1
sabashukvaniii <sshukvanii@gmail.com>
2024-06-27 20:18:22 +0400

    Add files via upload

---
+++ level1/lesson 1.py
@@ -0,0 +1,45 @@
+from turtle import *
+width(5)
+color("red")
+forward(100)
+left(90)
+
+forward(100)
+left(90)
+
+forward(100)
+left(90)
+
+forward(100)
+left(90)
+#the proces of making a door
+forward(30)
+color("brown")
+begin_fill()
+left(90)
+
+forward(40)
+right(90)
+
+forward(40)
+right(90)
+
+forward(40)
+end_fill()
+#i have made the house dachi
+#and i olso drew the door
+
+penup()
+goto(100, 100)
+pendown()
+
+color("pink")
+begin_fill()
+right(150)
+forward(100)
+
+left(120)
+forward(100)
+end_fill()
+
+exitonclick()
