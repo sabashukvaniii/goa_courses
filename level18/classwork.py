76d961902bb752ec34b8035e43599289e9ec66e1
sabashukvaniii <sshukvanii@gmail.com>
2024-07-16 12:41:01 +0400

    Add files via upload

---
+++ level18/classwork.py
@@ -0,0 +1,39 @@
+#number1
+# def greating():
+#     print("hello" + " " + name)
+
+# name = input()
+
+# greating()
+
+
+#number2
+# def three_sum():
+#     print(num1 + num2 + num3)
+
+# num1=int(input())
+# num2=int(input())
+# num3=int(input())  
+
+# three_sum()
+
+
+#number4
+
+# def profesion():
+#     print("my name is" + " " + name + " " + "my surname is" + " " + surname + " " +"i study at" + " " + academy)
+
+
+# name = str(input())
+# surname = str(input())
+# academy = str(input())
+
+# profesion()
+
+#number3
+
+# def start_end(start, end):
+#     for i in range(start, end):
+#         print(i)
+
+# start_end(1,100)
