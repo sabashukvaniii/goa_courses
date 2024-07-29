7f24e04e61083432af3e15cd0813d516b2975b46
sabashukvaniii <sshukvanii@gmail.com>
2024-07-29 17:21:25 +0400

    Add files via upload

---
+++ level20/homework.py
@@ -0,0 +1,90 @@
+# number 1
+
+# num1 = 10
+# num2 = 10
+# print(num1 + num2)
+
+#number 2
+# num1 = 10
+# num2 = 10
+# print(str(num1) + str(num2))
+
+#number 3
+
+# num2 = 20
+# num1 = 10
+# print(num2 / num1)
+
+#it is called implicit because it is assumed by the creator of this code that when you devide the numbers you get a float
+
+#number 4
+
+# num1 = 5
+# num2 = 6
+
+# print(num1 > num2)
+
+# num1 = 5
+# num2 = 6
+
+# print(num1 < num2)
+
+# num1 = 5
+# num2 = 6
+
+# print(num1 == num2)
+
+
+
+
+# print(num1 == num2)
+
+#number 5
+
+# print(5 + 5 == 6 + 4)
+
+# print(5 + 5 > 2 + 2)
+
+# print(5 + 5 < 2 + 2)
+
+# print(5 + 5 == 11)
+
+#number 6
+
+# print(True or False)
+
+# print(True or True)
+
+# print(False or False)
+
+# print(False or True)
+
+# print(False and False)
+
+# print(False and True)
+
+# print(True and False)
+
+#number 7
+
+# print(5 == 5 and 5 == 5)
+
+# print(5 == 5 and 5 == 6)
+
+# print(5 == 5 or 5 == 6)
+
+# print(5 == 6 or 5 == 7)
+
+#number 8
+
+# numbers = (1,2,3,4,5,6,7,8,9,10)
+# for i in numbers:
+#     print(i)
+
+#number 9 
+
+# listn = [1,2,3,4,5,6,7,8,9,10]
+# i = 0
+# for i in listn:
+#     print(i)
+#     i = i + 1
