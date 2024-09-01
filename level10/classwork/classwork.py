ad9a1a8e91e71e6c0adfba6691448d30c93abadc
sabashukvaniii <sshukvanii@gmail.com>
2024-09-01 20:07:01 +0400

    'new'

--- level10/classwork/classwork.py
+++ level10/classwork/classwork.py
@@ -2,4 +2,3 @@ num = "hello world"
 
 for helloworld in range (1 , 101):
     print("hello world")
-
---
+++ level24/homework.py
@@ -0,0 +1,5 @@
+def manual_abs(num1):
+    print(abs(num1))
+#abs brings out the positive number it is always a positive
+
+print(manual_abs(-123))
