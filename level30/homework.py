d18c8d1efc621b29379d3ad742cbf810161a65b9
sabashukvaniii <sshukvanii@gmail.com>
2024-09-19 21:19:14 +0400

    'new'

--- level30/homework.py
+++ level30/homework.py
@@ -1,1 +1,12 @@
-# codewarsebi gvqonda
+# dict = {
+#      1: ["mustang"], 
+#      2: [1000 horse power ] 
+#      3: [10000 dollars]
+# }
+
+
+# dict = {
+#      1: ["lamborghini"], 
+#      2: [1200 horse power ] 
+#      3: [100000 dollars]
+# }
--- level34/homework/homework.py
+++ level34/homework/homework.py
@@ -1,1 +1,30 @@
-#solo learns and codewars
+# name = "saba"
+# surnameee = "shukvani"
+# try:
+#     print(name + surname1)
+
+# except NameError:
+#     print("it failed")
+
+
+# listn = [1,2,3,4,5,6,7,8,9,10]
+# try:
+#     print(listn[100])
+
+# except IndexError:
+#     print("it does not excist")
+
+
+# a = 10000
+# b = "sabashukvani"
+# try:
+#     print(str(a))
+
+#     print(float(b))
+
+# except ValueError:
+#     print("it failed")
+
+
+
+
