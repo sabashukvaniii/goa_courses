16f259ec1cb4fea0dc7a3b8fbb795b28e220a8be
sabashukvaniii <sshukvanii@gmail.com>
2024-11-29 23:43:26 +0400

    new

---
+++ level55/homework/xcript.js
@@ -0,0 +1,9 @@
+// Prompt the user for two numbers
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+
+// Calculate the sum
+let sum = num1 + num2;
+
+// Output the sum
+alert("The sum of the two numbers is: " + sum);
